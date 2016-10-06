fd3b51e5c9b0ff71874941073798c9f195cd3556
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-07 06:06:37 +0800

    Update

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -1,38 +1,38 @@
 // customize.js
 
 $(document)
-  .ready(function() {
+    .ready(function() {
 
-    // fix menu when passed
-    $('.masthead')
-      .visibility({
-        once: false,
-        onBottomPassed: function() {
-          $('.fixed.menu').transition('fade in');
-        },
-        onBottomPassedReverse: function() {
-          $('.fixed.menu').transition('fade out');
-        }
-      });
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function() {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function() {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
 
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item');
+        // create sidebar and attach to menu open
+        $('.ui.sidebar')
+            .sidebar('attach events', '.toc.item');
 
-  });
+    });
 
 // copyright
 $('#copyright')
-  .html(
-    function() {
-      var date = new Date();
-      return '&copy; ' + date.getFullYear() + ' TEELAB';
-    }
-  );
+    .html(
+        function() {
+            var date = new Date();
+            return '&copy; ' + date.getFullYear() + ' TEELAB';
+        }
+    );
 
 // footer feedback
 $('#feedback-footer-link')
-  .on('click', function() {
-    $('#contacts-dimmer')
-      .dimmer('show');
-  });
+    .on('click', function() {
+        $('#contacts-dimmer')
+            .dimmer('show');
+    });
