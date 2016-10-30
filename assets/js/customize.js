68e3f030a5f292ba05ed20e5b81edfd7da7dda3d
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-31 03:23:55 +0800

    Update

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -57,15 +57,50 @@ $('.loading-trigger')
 
 // analytics
 function updateAnalytics() {
-    var todayIP = 0;
-    var todayPV = 0;
-    $.getJSON('https://vps.irockbunny.com/analytics/?module=API&method=VisitsSummary.getUniqueVisitors&idSite=1&period=day&date=today&format=JSON&token_auth=f415eff9302de22d195d1d87b092c825', function(data) {
-        todayIP = data.value;
+    $.getJSON('https://vps.irockbunny.com/analytics/?callback=?', {
+        'module': 'API',
+        'method': 'VisitsSummary.getUniqueVisitors',
+        'idSite': '1',
+        'period': 'day',
+        'date': 'today',
+        'format': 'JSON',
+        'token_auth': 'f415eff9302de22d195d1d87b092c825'
+    }, function(data) {
+        $('#today-ip').text(data.value);
     });
-    $.getJSON('https://vps.irockbunny.com/analytics/?module=API&method=VisitsSummary.getVisits&idSite=1&period=day&date=today&format=JSON&token_auth=f415eff9302de22d195d1d87b092c825', function(data) {
-        todayPV = data.value;
+    $.getJSON('https://vps.irockbunny.com/analytics/?callback=?', {
+        'module': 'API',
+        'method': 'VisitsSummary.getVisits',
+        'idSite': '1',
+        'period': 'day',
+        'date': 'today',
+        'format': 'JSON',
+        'token_auth': 'f415eff9302de22d195d1d87b092c825'
+    }, function(data) {
+        $('#today-pv').text(data.value);
     });
-    $('#analytics').html('今日IP：' + todayIP + ' &middot; 今日PV：' + todayPV);
+    $.getJSON('https://vps.irockbunny.com/analytics/?callback=?', {
+        'module': 'API',
+        'method': 'VisitsSummary.getUniqueVisitors',
+        'idSite': '1',
+        'period': 'day',
+        'date': 'yesterday',
+        'format': 'JSON',
+        'token_auth': 'f415eff9302de22d195d1d87b092c825'
+    }, function(data) {
+        $('#yesterday-ip').text(data.value);
+    });
+    $.getJSON('https://vps.irockbunny.com/analytics/?callback=?', {
+        'module': 'API',
+        'method': 'VisitsSummary.getVisits',
+        'idSite': '1',
+        'period': 'day',
+        'date': 'yesterday',
+        'format': 'JSON',
+        'token_auth': 'f415eff9302de22d195d1d87b092c825'
+    }, function(data) {
+        $('#yesterday-pv').text(data.value);
+    });
 };
 updateAnalytics();
 setInterval(updateAnalytics, 15000);
