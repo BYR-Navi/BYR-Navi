90cd90318f30b16cd3c2c3e7af2b62de61e44985
iRB Air <irockbunny@outlook.com>
2017-02-26 23:23:37 +0800

    Update

--- assets/js/customize-home.js
+++ assets/js/customize-home.js
@@ -20,7 +20,7 @@ $('#page-loading-progress').progress({
 // version
 function updateVersion(updateProgressBar) {
     $.getJSON('https://api.github.com/repos/iROCKBUNNY/BYR-Navi', function(data) {
-        $('#version img').attr('src', 'https://img.shields.io/badge/Updated-' + encodeURIComponent(moment(data.pushed_at).fromNow()) + '-orange.svg');
+        $('#version img').attr('src', 'https://img.shields.io/badge/Update-' + encodeURIComponent(moment(data.pushed_at).fromNow()) + '-brightgreen.svg');
         if (updateProgressBar) {
             $('#page-loading-progress').progress('increment');
         };
