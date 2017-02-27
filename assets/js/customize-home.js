1ed268929ec69473643ae64641322b199ea3ae86
iRB Air <irockbunny@outlook.com>
2017-02-27 13:21:50 +0800

    Update

--- assets/js/customize-home.js
+++ assets/js/customize-home.js
@@ -17,6 +17,17 @@ $('#page-loading-progress').progress({
     }
 });
 
+// GitHub
+function updateGitHub() {
+    $('#github-watch img').attr('src', 'https://img.shields.io/github/watchers/iROCKBUNNY/BYR-Navi.svg?style=social&label=Watch');
+    $('#github-star img').attr('src', 'https://img.shields.io/github/stars/iROCKBUNNY/BYR-Navi.svg?style=social&label=Star');
+    $('#github-fork img').attr('src', 'https://img.shields.io/github/forks/iROCKBUNNY/BYR-Navi.svg?style=social&label=Fork');
+};
+updateGitHub();
+setInterval(function() {
+    updateGitHub();
+}, 15000);
+
 // version
 moment.locale('zh-cn');
 var pushedAt = 'loading';
