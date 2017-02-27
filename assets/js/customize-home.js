0c0f10e34360d17f09b5e09a3ad0db48d92d19d4
iRB Air <irockbunny@outlook.com>
2017-02-27 13:05:11 +0800

    Update

--- assets/js/customize-home.js
+++ assets/js/customize-home.js
@@ -32,7 +32,7 @@ function updateVersion(pushedAt) {
         thisMoment = encodeURIComponent(moment(pushedAt).fromNow());
         $('#version img').attr('src', 'https://img.shields.io/badge/' + encodeURIComponent('更新') + '-' + thisMoment + '-brightgreen.svg').ready(function() {
             if (thisMoment !== lastMoment) {
-                $(this).transition('pulse');
+                $('#version img').transition('pulse');
                 lastMoment = thisMoment;
             };
         });
