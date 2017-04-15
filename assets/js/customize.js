34655a8d5aa3614cd3656df8b5c4ea427ddaaeaf
cainiu <i@cainiu.de>
2017-04-15 17:03:45 +0800

    百度统计

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -1,3 +1,12 @@
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?72e6641dc820f9f3787c7d701f0635cf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
 // customize.js
 
 $(document).ready(function () {
