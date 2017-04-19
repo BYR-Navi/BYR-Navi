63d916d858c50f2b1f4b8e7421bc4ceb1dd685a2
Cain <i@cainiu.de>
2017-04-19 19:16:19 +0800

    Update customize.js

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -24,6 +24,17 @@ $('.loading-trigger').on('click', function () {
 $('.ui.inverted.masthead.segment').addClass('bg' + Math.ceil(Math.random() * 14)).removeClass('zoomed');
 
 // analytics
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?72e6641dc820f9f3787c7d701f0635cf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
 // $.getJSON(analyticsAPI.url, {
 //     'module': 'API',
 //     'method': 'VisitsSummary.getVisits',
