7d00e97971b30ba3244919b19880f78bb0bbccf9
iRB Air <irockbunny@outlook.com>
2017-05-04 23:12:54 +0800

    Update

--- assets/js/customize-redirect.js
+++ assets/js/customize-redirect.js
@@ -1,7 +1,9 @@
 // customize-redirect.js
 
+var service = $.url('?service');
+var query = $.url('?query');
 var redirectUrl = $.url('?next');
 if (redirectUrl) {
-    $('#redirect-url').text(redirectUrl);
+    $('#redirect-url').text('正在跳转到页面').append($('<a>').addClass('sub header').attr('href', redirectUrl).text(service + '：' + query));
     window.location.replace(redirectUrl);
 };
