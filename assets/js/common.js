218bed48868450bc8bd5e6ea3effde4e21292345
iROCKBUNNY <irockbunny@outlook.com>
2017-10-28 21:26:02 +0800

    Update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,18 +1,21 @@
 // common.js
 
 $(document).ready(function () {
-    // fix menu when passed
-    $('.masthead').visibility({
-        once: false,
-        onBottomPassed: function () {
-            $('.fixed.menu').transition('fade in');
-        },
-        onBottomPassedReverse: function () {
-            $('.fixed.menu').transition('fade out');
-        }
-    });
-    // create sidebar and attach to menu open
-    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    if ($(window).width() >= 768) {
+        // fix menu when passed
+        $('.masthead').visibility({
+            once: false,
+            onBottomPassed: function () {
+                $('.fixed.menu').transition('fade in');
+            },
+            onBottomPassedReverse: function () {
+                $('.fixed.menu').transition('fade out');
+            }
+        });
+    } else {
+        // create sidebar and attach to menu open
+        $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    };
 });
 
 // loading dimmer
