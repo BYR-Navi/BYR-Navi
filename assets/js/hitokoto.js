f9e13032ab4b728bd69e2165bb08e94b7b5b8c15
iROCKBUNNY <irockbunny@outlook.com>
2017-12-10 11:16:20 +0800

    Update

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -1,6 +1,6 @@
 // hitokoto.js
 
-$.getJSON('https://sslapi.hitokoto.cn/', function (data) {
+$.getJSON('http://api.hitokoto.cn/', function (data) {
     $('#hitokoto').attr({
         'href': '//hitokoto.cn?id=' + data.id,
         'target': '_blank'
