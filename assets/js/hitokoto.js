1511d10544eb75bfcbcf690427053cc5fc5f8209
iROCKBUNNY <irockbunny@outlook.com>
2019-11-23 18:12:19 +0800

    Update

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -7,4 +7,9 @@ $.getJSON('https://v1.hitokoto.cn', function (data) {
     }).append(data.hitokoto).append(
         $('<div>').addClass('detail').text(data.from)
     );
+}).fail(function () {
+    $('#hitokoto').attr({
+        'href': 'https://www.y-english.org/',
+        'target': '_blank'
+    }).html('<i class="lightbulb icon"></i>不背单词的GRE工程化解决方案<div class="detail">云英语</div>');
 });
